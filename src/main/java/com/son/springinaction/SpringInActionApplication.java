7b27e9ea28ed74b106aa6db4c64c60c78722eaa0
Chaegeon <thscorjs1002@naver.com>
2020-12-11 18:13:02 +0900

    Project

---
+++ src/main/java/com/son/springinaction/SpringInActionApplication.java
@@ -0,0 +1,13 @@
+package com.son.springinaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInActionApplication.class, args);
+    }
+
+}
