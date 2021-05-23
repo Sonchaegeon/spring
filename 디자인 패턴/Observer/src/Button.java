de72e2265d0f416adbe1e0a6d6a60567e95a79fb
sonchaegeon <thscorjs1002@naver.com>
2021-05-23 19:27:42 +0900

    CREATE; Observer classes

---
+++ 디자인 패턴/Observer/src/Button.java
@@ -0,0 +1,19 @@
+package Observer.src;
+
+public class Button {
+
+    private String name;
+    private IButtonListener buttonListener;
+
+    public Button(String name) {
+        this.name = name;
+    }
+
+    public void click(String message) {
+        buttonListener.clickEvent(message);
+    }
+
+    public void addListener(IButtonListener buttonListener) {
+        this.buttonListener = buttonListener;
+    }
+}
---
+++ 디자인 패턴/Observer/src/IButtonListener.java
@@ -0,0 +1,5 @@
+package Observer.src;
+
+public interface IButtonListener {
+    void clickEvent(String event);
+}
