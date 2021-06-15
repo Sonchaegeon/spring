bc63ecfab55b7877b2b84781ef54059e52fbfbc2
sonchaegeon <thscorjs1002@naver.com>
2021-06-15 14:03:11 +0900

    365p. ver2 deploy

--- book/scripts/deploy.sh
+++ book/scripts/deploy.sh
@@ -7,21 +7,21 @@ echo "> Build 파일 복사"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> 현재 구동 중인 애플리케이션 pid 확인"
+echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl aws-springboot-book | grep jar | awk '{print #!}')
+CURRENT_PID=$(pgrep -fl aws-springboot-book | grep jar | awk '{print $1}')
 
-echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 5
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
 fi
 
-echo "> 새 애플리케이션 배포"
+echo "> 새 어플리케이션 배포"
 
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
@@ -34,6 +34,6 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-  -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
-  -Dspring.profiles.active=real \
-  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+    -Dspring.profiles.active=real \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
